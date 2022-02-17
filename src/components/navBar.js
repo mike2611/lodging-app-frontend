0318b44f6f90ec091c46973402584b72b0d2ff97
mike2611 <mpuentes2611@gmail.com>
2022-02-17 12:09:24 -0600

    Add router to navBar

---
+++ src/components/navBar.js
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route } from 'react-router';
+
+const NavBar = () => (
+  <Router>
+    <Routes>
+      {/* <Route exact path="/" element={</>} /> */}
+    </Routes>
+  </Router>
+);
