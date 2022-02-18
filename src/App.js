b7283709e12c5e9e090ccf3434b1627b83267e50
nachosala89 <nachosala89@gmail.com>
2022-02-18 19:50:21 -0300

    Create basic structure for details page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Details from './components/Details';
 
-const App = () => {
-  <h1>Configuration</h1>;
-};
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/room/:roomId" exact element={<Details />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
