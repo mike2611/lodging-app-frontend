a20049563c855a9027a341c5096dcd7fabd179ea
mike2611 <mpuentes2611@gmail.com>
2022-02-22 11:17:26 -0600

    Add styles to navBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
-import Main from './components/Main';
+import NavBar from './components/NavBar';
 import store from './redux/store';
 
 const App = () => (
   <Provider store={store}>
-    <Main />
+    <NavBar />
   </Provider>
 );
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,36 @@
-import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, NavLink } from 'react-router-dom';
 import { Route } from 'react-router';
+import Main from './Main';
+import logo from '../assets/logo.png';
 
 const NavBar = () => (
   <Router>
-    <Routes>
-      {/* <Route exact path="/" element={</>} /> */}
-    </Routes>
+    <div className="nav">
+      <div className="nav-bar d-flex">
+        <span><img className="main-logo" src={logo} alt="logo" /></span>
+        <ul className="nav-menu d-flex">
+          <li className="mb-3">
+            <NavLink to="/" className={({ isActive }) => (isActive ? 'active-style px-5 py-2' : 'nav-elements')}> Home </NavLink>
+          </li>
+          <li className="mb-3">
+            <NavLink to="/reserve" className={({ isActive }) => (isActive ? 'active-style px-5 py-2' : 'nav-elements')}>Reserve</NavLink>
+          </li>
+          <li className="mb-3">
+            <NavLink to="/reservations" className={({ isActive }) => (isActive ? 'active-style px-5 py-2' : 'nav-elements')}>My reservations</NavLink>
+          </li>
+          <li className="mb-3">
+            <NavLink to="/add" className={({ isActive }) => (isActive ? 'active-style px-5 py-2' : 'nav-elements')}>Add room</NavLink>
+          </li>
+          <li className="mb-3">
+            <NavLink to="/delete" className={({ isActive }) => (isActive ? 'active-style px-5 py-2' : 'nav-elements')}>Delete room</NavLink>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+      </Routes>
+    </div>
   </Router>
 );
+
+export default NavBar;
