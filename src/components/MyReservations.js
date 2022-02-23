27f99a5b834f5ef70af59355d5b863d1d16a454a
nachosala89 <nachosala89@gmail.com>
2022-02-23 11:57:16 -0300

    Sort reservations by dates

--- src/components/MyReservations.js
+++ src/components/MyReservations.js
@@ -50,8 +50,8 @@ const MyReservations = () => {
     {
       id: 1,
       room: 0,
-      checkin_date: '2022-03-05',
-      checkout_date: '2022-03-15',
+      checkin_date: '2022-02-22',
+      checkout_date: '2022-03-10',
     },
     {
       id: 2,
@@ -79,34 +79,84 @@ const MyReservations = () => {
     const diffDays = checkout.diff(checkin, 'days');
     reservations[index] = { ...reservation, nights: diffDays };
   });
-  // const current = reservations.filter(reserv => )
 
-  // const today = new Date().toISOString().slice(0, 10);
+  const currentReserv = [];
+  const pastReserv = [];
+  const futureReserv = [];
+  const today = new Date().toISOString().slice(0, 10);
+  reservations.forEach((reservation) => {
+    if (reservation.checkin_date <= today && reservation.checkout_date >= today) {
+      currentReserv.push(reservation);
+    } else if (reservation.checkout_date < today) {
+      pastReserv.push(reservation);
+    } else {
+      futureReserv.push(reservation);
+    }
+  });
 
   return (
     <section className="container">
       <h1>My Reservations</h1>
-      <br />
-      <ul>
-        {reservations.map((reservation) => (
-          <li key={reservation.id} className="row">
-            <Reservation
-              image={rooms[reservation.room].image}
-              hotel={hotels[rooms[reservation.room].hotel].name}
-              city={hotels[rooms[reservation.room].hotel].city}
-              address={hotels[rooms[reservation.room].hotel].address}
-              description={rooms[reservation.room].description}
-              checkin={reservation.checkin_date}
-              checkout={reservation.checkout_date}
-              price={rooms[reservation.room].price}
-              nights={reservation.nights}
-            />
-          </li>
-        ))}
-      </ul>
       <h2>Current</h2>
+      <ul>
+        {(currentReserv.length === 0)
+          ? <p>You have no current reservations.</p>
+          : currentReserv.map((reservation) => (
+            <li key={reservation.id} className="row">
+              <Reservation
+                image={rooms[reservation.room].image}
+                hotel={hotels[rooms[reservation.room].hotel].name}
+                city={hotels[rooms[reservation.room].hotel].city}
+                address={hotels[rooms[reservation.room].hotel].address}
+                description={rooms[reservation.room].description}
+                checkin={reservation.checkin_date}
+                checkout={reservation.checkout_date}
+                price={rooms[reservation.room].price}
+                nights={reservation.nights}
+              />
+            </li>
+          ))}
+      </ul>
       <h2>Upcoming</h2>
+      <ul>
+        {(futureReserv.length === 0)
+          ? <p>You have no upcoming reservations.</p>
+          : futureReserv.map((reservation) => (
+            <li key={reservation.id} className="row">
+              <Reservation
+                image={rooms[reservation.room].image}
+                hotel={hotels[rooms[reservation.room].hotel].name}
+                city={hotels[rooms[reservation.room].hotel].city}
+                address={hotels[rooms[reservation.room].hotel].address}
+                description={rooms[reservation.room].description}
+                checkin={reservation.checkin_date}
+                checkout={reservation.checkout_date}
+                price={rooms[reservation.room].price}
+                nights={reservation.nights}
+              />
+            </li>
+          ))}
+      </ul>
       <h2>Past</h2>
+      <ul>
+        {(pastReserv.length === 0)
+          ? <p>You have no past reservations.</p>
+          : pastReserv.map((reservation) => (
+            <li key={reservation.id} className="row">
+              <Reservation
+                image={rooms[reservation.room].image}
+                hotel={hotels[rooms[reservation.room].hotel].name}
+                city={hotels[rooms[reservation.room].hotel].city}
+                address={hotels[rooms[reservation.room].hotel].address}
+                description={rooms[reservation.room].description}
+                checkin={reservation.checkin_date}
+                checkout={reservation.checkout_date}
+                price={rooms[reservation.room].price}
+                nights={reservation.nights}
+              />
+            </li>
+          ))}
+      </ul>
     </section>
   );
 };
