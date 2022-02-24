8ada45683df7fd07a05743125f06ca3f22b74900
nachosala89 <nachosala89@gmail.com>
2022-02-24 14:54:32 -0300

    Consume API from MyReservations component

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,12 @@ import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import roomsReducer from './rooms/roomsReducer';
 import loginReducer from './login/loginReducer';
+import reservationsReducer from './reservations/reservationsReducer';
 
 const reducer = combineReducers({
   roomsReducer,
   loginReducer,
+  reservationsReducer,
 });
 
 const store = createStore(
