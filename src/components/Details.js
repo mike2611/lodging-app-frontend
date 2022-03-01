c51f85440085e3cfd87d56ca1acd5e3af33f019e
nachosala89 <nachosala89@gmail.com>
2022-03-01 16:36:29 -0300

    First changes to Details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -9,48 +9,50 @@ const Details = () => {
   const room = rooms.find((room) => room.id === parseInt(id, 10));
 
   return (
-    <section className="container">
-      {(room === undefined)
-        ? <h1>The requested room doesn&apos;t exist. Please go back and try with another.</h1>
-        : (
-          <div className="row">
-            <div className="col-md-8 text-center">
-              <img src={room.image} alt="Room" className="w-75" />
+    <section className="d-flex align-items-center w-100 vh-100">
+      <div className="container">
+        {(room === undefined)
+          ? <h1>The requested room doesn&apos;t exist. Please go back and try with another.</h1>
+          : (
+            <div className="row">
+              <div className="col-md-8 text-center">
+                <img src={room.image} alt="Room" className="w-75" />
+              </div>
+              <div className="col-md-4">
+                <h2>{room.hotel.name}</h2>
+                <p>{room.description}</p>
+                <Table striped hover>
+                  <tbody>
+                    <tr>
+                      <td>Max. amount of people</td>
+                      <td>{room.people_amount}</td>
+                    </tr>
+                    <tr>
+                      <td>Price per night</td>
+                      <td>{`$${room.price}`}</td>
+                    </tr>
+                    <tr>
+                      <td>Price per person</td>
+                      <td>{`$${(room.price / room.people_amount).toFixed(2)}`}</td>
+                    </tr>
+                  </tbody>
+                </Table>
+                <p>
+                  <strong>{room.hotel.city.name}</strong>
+                  ,
+                  {' '}
+                  {room.hotel.address}
+                </p>
+                <Link to="/">
+                  <p>DISCOVER MORE LODGING PLACES</p>
+                </Link>
+                <Link to={`/room/${room.id}/reserve`}>
+                  Reserve
+                </Link>
+              </div>
             </div>
-            <div className="col-md-4">
-              <h2>{room.hotel.name}</h2>
-              <p>{room.description}</p>
-              <Table striped hover>
-                <tbody>
-                  <tr>
-                    <td>Max. amount of people</td>
-                    <td>{room.people_amount}</td>
-                  </tr>
-                  <tr>
-                    <td>Price per night</td>
-                    <td>{`$${room.price}`}</td>
-                  </tr>
-                  <tr>
-                    <td>Price per person</td>
-                    <td>{`$${(room.price / room.people_amount).toFixed(2)}`}</td>
-                  </tr>
-                </tbody>
-              </Table>
-              <p>
-                <strong>{room.hotel.city.name}</strong>
-                ,
-                {' '}
-                {room.hotel.address}
-              </p>
-              <Link to="/">
-                <p>DISCOVER MORE LODGING PLACES</p>
-              </Link>
-              <Link to={`/room/${room.id}/reserve`}>
-                Reserve
-              </Link>
-            </div>
-          </div>
-        )}
+          )}
+      </div>
     </section>
   );
 };
